b18a054bde262992e5abf3933fef96dc8feb4463
Rahid Aliyev <rahidaliyev18@gmail.com>
2022-08-05 17:37:01 +0400

    first commit

---
+++ src/components/ReadOnlyRow.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ReadOnlyRow = ({ contact, handleEditClick, handleDeleteClick }) => {
+  return (
+    <tr>
+      <td>{contact.fullName}</td>
+      <td>{contact.address}</td>
+      <td>{contact.phoneNumber}</td>
+      <td>{contact.email}</td>
+      <td>
+        <button
+        id="edit"
+          type="button"
+          onClick={(event) => handleEditClick(event, contact)}
+        >
+          Edit
+        </button>
+        <button
+        id="delete" type="button" onClick={() => handleDeleteClick(contact.id)}>
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default ReadOnlyRow;
